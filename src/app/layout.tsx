0e003efde50af3ea7880f9fe98172dd493e1cf22
sahadev jaat <sahadevjaat@gmail.com>
2025-11-29 13:55:35 +0000

    lock the zoom in app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
@@ -6,6 +6,13 @@ export const metadata: Metadata = {
   title: 'Priyanka Granite Sheet',
   description: 'An app for granite length and width measurements in inches.',
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 export default function RootLayout({
   children,
