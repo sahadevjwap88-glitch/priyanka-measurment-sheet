06c59c79bc169d5551886f791d8fef816e0c3456
sahadev jaat <sahadevjaat@gmail.com>
2025-12-03 13:49:58 +0000

    remove the horizontal scroll bar from page of measurement sheet

--- src/components/granite-table.tsx
+++ src/components/granite-table.tsx
@@ -130,7 +130,7 @@ export function GraniteTable({ fields, register, errors, control, setValue, shee
   };
 
   return (
-    <div className="rounded-md border">
+    <div className="rounded-md border overflow-x-hidden">
       <Table>
         <TableHeader>
           <TableRow>
