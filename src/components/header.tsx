83976942d55a362d414696594d0b33e7747ec7df
sahadev jaat <sahadevjaat@gmail.com>
2025-12-14 08:27:41 +0000

    remove (_for element <Header>_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -36,7 +36,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
         <Link href="/" className="mr-auto">
-          <h1 className="text-xl font-bold">Priyanka Granites</h1>
+          
         </Link>
         <div className="flex items-center space-x-4">
           {!isUserLoading && (
