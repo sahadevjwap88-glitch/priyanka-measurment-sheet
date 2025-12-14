1ab43c18b3b9f3a78e1778c8163342c4ceb1faa2
sahadev jaat <sahadevjaat@gmail.com>
2025-12-14 09:06:36 +0000

    remove (_for element <Header>_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase';
-import Header from '@/components/header';
 
 export const metadata: Metadata = {
   title: 'Priyanka Granite Sheet',
@@ -33,7 +32,6 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
-          <Header />
           <div className="pb-20">
             {children}
           </div>
