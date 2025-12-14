a950aa27b3a7241d59dc2661b47ab5d754184d56
sahadev jaat <sahadevjaat@gmail.com>
2025-12-14 09:00:24 +0000

    remove button (_for element <Primitive.span>_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -44,40 +44,7 @@ export default function Header() {
     }
 
     if (user) {
-      return (
-         <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-               <Avatar className="h-8 w-8">
-                 <AvatarFallback>
-                    <UserIcon className="h-5 w-5" />
-                 </AvatarFallback>
-               </Avatar>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56" align="end" forceMount>
-            <DropdownMenuLabel className="font-normal">
-              <div className="flex flex-col space-y-1">
-                <p className="text-sm font-medium leading-none">My Account</p>
-                <p className="text-xs leading-none text-muted-foreground">
-                  {user.email}
-                </p>
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-             <DropdownMenuItem asChild>
-                <Link href="/account">
-                  <UserIcon className="mr-2 h-4 w-4" />
-                  <span>Account</span>
-                </Link>
-              </DropdownMenuItem>
-            <DropdownMenuItem onClick={handleSignOut}>
-              <LogOut className="mr-2 h-4 w-4" />
-              <span>Log out</span>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
+      return null;
     }
 
     return (
